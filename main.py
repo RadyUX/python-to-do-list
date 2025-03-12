67b3dd1ba043e27ebd4b95c77f55832547f8d9a3
rady <rafaele.sinaguglia@gmail.com>
2025-03-12 09:33:05 +0100

    commentaire

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from tkinter import *
 
 from tkinter import messagebox
 
-
 # Back end 
+task_list = []
+
+counter = 1
+
+
 
 ## gestion des erreurs
 
@@ -25,9 +29,6 @@ def afficher_tache():
 
 
 # front end
-task_list = []
-
-counter = 1
 
 
 
@@ -44,25 +45,34 @@ if __name__ == "__main__" :
      #resolution de la fenetre
      gui.geometry("250x300")
 
-
+    # couleur de fond
      gui.configure(background="light green")
 
+     # cree une boite de texte pour entré une nouvelle tache
      enterTaskField = Entry(gui)
-     
+
+
+     # cree un label entrez votre taches
      enterTask = Label(gui, text = "Enter Your Task", bg = "light green")
  
-
+    # bouton pour supprimer la tache
      delete = Button(gui, text="delete", fg = "black", bg = "red", command = delete)
+     # bouton pour quitter l'application
      Exit = Button(gui, text = "Exit", fg = "Black", bg = "Red", command = exit)
-
+     #label qui dit de selectionner un chiffre
      taskNumber = Label(gui, text = "delete task number", bg = "blue")
 
+     # champ pour entrer le chiffre
      taskNumberField = Text(gui, height= 1, width=2, font="lucida 13")
-
+     # rentrer le contenu de la tache
      TextArea = Text(gui, height = 5, width = 25, font = "lucida 13")
 
+
+     # la tache est afficher dans la liste apres actionner ce bouton
      Submit = Button(gui, text = "Submit", fg = "Black", bg = "Red", command = afficher_tache)
-     
+
+
+     ## affiche les label et champ de text 
      delete.grid(row = 6, column = 2, pady = 5)
 
      TextArea.grid(row = 3, column = 2, padx = 10, sticky = W)
